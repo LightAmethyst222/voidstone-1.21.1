14198aeba75c247d4aea7f45b4eb05c0cb345df8
LightAmethyst222 <2wqlucas@gmail.com>
2024-11-12 13:50:26 -0500

    Initial commit

---
+++ src/main/java/net/lightamethyst222/voidstone/Voidstone.java
@@ -0,0 +1,16 @@
+package net.lightamethyst222.voidstone;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Voidstone implements ModInitializer {
+	public static final String MOD_ID = "voidstone";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lightamethyst222/voidstone/VoidstoneClient.java
@@ -0,0 +1,10 @@
+package net.lightamethyst222.voidstone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VoidstoneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
